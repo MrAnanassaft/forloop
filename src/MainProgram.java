196668b917652ef4ffb0eedaf67ae8abdaba4754
alex2 <alex20jiang07@gmail.com>
2023-04-18 21:49:58 +0200

    Hihihiha

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,7 +1,26 @@
 public class MainProgram {
     public static void main(String[] args) {
+        //printSomeNumbers();
+        printNumbers(20,30);
 
-
+        System.out.println(sumUp(20,30));
     }
-    public
+    public static void printSomeNumbers(){
+        for(int i = 6; i < 24; i++){
+            System.out.println(i);
+        }
+    }
+    public static void printNumbers(int start, int end){
+        for(int i = start; i < end+1; i++){
+            System.out.println(i);
+        }
+    }
+    public static int sumUp(int start, int end){
+        int a = 0;
+        for(int i = start; i < end+1; i++){
+            a = a + i;
+        }
+        return a;
+    }
+
 }
