85aeb48a4ea5673b34d709883dc9b5e25df306c6
alex2 <alex20jiang07@gmail.com>
2023-04-25 08:25:28 +0200

    Hihihiha

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,9 +1,11 @@
 public class MainProgram {
     public static void main(String[] args) {
-        //printSomeNumbers();
+        printSomeNumbers();
         printNumbers(20,30);
 
         System.out.println(sumUp(20,30));
+        System.out.println(powerOfTwo(2));
+        System.out.println(sumUpPowerOfTwo(3));
     }
     public static void printSomeNumbers(){
         for(int i = 6; i < 24; i++){
@@ -22,5 +24,28 @@ public class MainProgram {
         }
         return a;
     }
+
+    public static int powerOfTwo(int p){
+        if(p >= 0) {
+            int number = 1;
+            for (int i = 0; i < p; i++) {
+                number = number * 2;
+            }
+            return number;
+        }else{
+            System.out.println("↓ this number must be greater or equal to 0");
+            return p;
+        }
+    }
+
+    public static int sumUpPowerOfTwo(int n){
+        int end = 0;
+        for(int i = 0; i < n+1; i++){
+            end += powerOfTwo(i);
+        }
+        return end;
+    }
+
+
 
 }
